d97c402274c3e9990e757a4e5f7c078c6aa08d4f
Alexandra Ackerman <aaackerman@Alexandras-MBP.home>
2020-03-26 20:51:42 -0400

    Initialize appolo server with dummy clip data

---
+++ index.js
@@ -0,0 +1,35 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Clip {
+    title: String
+    author: String
+  }
+
+  type Query {
+    clips: [Clip]
+  }
+`;
+const clips = [
+	{
+			title: 'Harry Potter and the Chamber of Secrets',
+			author: 'J.K. Rowling',
+	},
+	{
+			title: 'Jurassic Park',
+			author: 'Michael Crichton',
+	},
+];
+
+const resolvers = {
+	Query: {
+			clips: () => clips,
+	},
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
